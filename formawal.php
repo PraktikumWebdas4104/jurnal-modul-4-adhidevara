7c780694e7f82798ed26e81d8a29ad1cdcfce7e7
adhidevara <43514620+adhidevara@users.noreply.github.com>
2018-09-28 09:53:55 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,57 @@
+<form method="POST"> <h1>LOGIN</h1>
+	<table>
+		<tr>
+			<td>USERNAME</td>
+			<td>:</td>
+			<td><input type="text" name="usr"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<td>:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td align="right"><input type="submit" name="submit" value="LOGIN"></td>
+		</tr>
+	</table>
+
 <?php
+	if (isset($_POST['submit'])) {
+		$usr = $_POST['usr'];
+		$pass = $_POST['pass'];
 
+		$login = array('usr' => 'avara',
+					   'pass' => 'avara');
+		
+		if ($usr==$login['usr'] && $pass==$login['pass']) {
+			echo "<h1>LOGIN SUKSES</h1>";
+?>
 
+<form method="POST">
+	<i><b>HOBI</b></i>
+		<table>
+			<tr>
+				<td>CODING <input type="checkbox" name="hobi[]" value="coding"></td>
+			</tr>
+			<tr>
+				<td>TRAVELING <input type="checkbox" name="hobi[]" value="traveling"></td>
+			</tr>
+			<tr>
+				<td>GAMING <input type="checkbox" name="hobi[]" value="gaming"></td>
+			</tr>
+			<tr>
+				<td>EDITING <input type="checkbox" name="hobi[]" value="editing"></td>
+				<td><input type="submit" name="kirc" value="Check"></td>
+			</tr>
+		</table>
+</form>
 
+<?php
+		}else{
+			echo "<h1>LOGIN GAGAL</h1>";
+		}
+	}
+		include 'proses.php';
 ?>
